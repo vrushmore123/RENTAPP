49f1d0d5dd185328cc38c32a24c8408f911a8b37
vrushmore123 <132755812+vrushmore123@users.noreply.github.com>
2024-12-28 17:22:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import UserNav from "./assets/UserNav.js"
+import MultiStepForm from "./pages/MultiStepForm.js"
+
+
+
+function App() {
+ 
+  return (
+    <div>
+      <Router>
+    
+          
+{/* 
+        {isAuthenticated && (
+          <>
+            {user.role === "admin" && <AdminNav />}
+            {user.role === "user" && <UserNav />}
+          </>
+        )} */}
+
+
+        <div>
+          <Routes>
+            {/* {!isAuthenticated && <Route path="/" element={<Login />} />} */}
+            <Route exact path="/" element={<MultiStepForm />} />
+          
+            {/* {isAuthenticated && totpVerified && user.is2FAEnabled ? (
+              <Route path="/" element={<TotpPage />} />
+            ) : (
+              isAuthenticated &&
+              !totpVerified && (
+                <>
+                  {user.role === "user" && (
+                    <>
+                      <Route exact path="/" element={<Home />} />
+                  
+                    </>
+                  )}
+
+                  {user.role === "admin" && (
+                    <>
+                      <Route exact path="/" element={<ProjectReferrals />} />
+                   
+                    </>
+                  )}
+
+                  {user.role === "superadmin" && (
+                    <>
+                      <Route exact path="/" element={<SuperAdminDashboard />} />
+                    </>
+                  )}
+                </>
+              )
+            )} */}
+
+           
+
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
